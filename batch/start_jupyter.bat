706b51fc02523188d423c281953365e0fc28e520
Nuno da Costa <nmcosta89@gmail.com>
2022-03-11 15:09:31 +0000

    update batch and json2mmaction

---
+++ batch/start_jupyter.bat
@@ -0,0 +1,17 @@
+REM START VIOLENT ACTION ENV
+
+@echo OFF
+title violent_action
+
+echo Hello! This is a batch to start violent_action jupyter.
+
+
+REM ACTIVATE ENV ********
+call C:\Tools\miniconda3\condabin\conda.bat activate violent_action
+
+REM change dir to start pyff *****
+cd "G:\My Drive\projects\bosch_P19\research\python_ws\molannotate"
+
+REM Start jupyter lab *******
+call jupyter lab
+
